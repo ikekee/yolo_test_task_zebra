8132e09e75ee5d38b1c82f4c33676a07b95bcca7
rafaelkadyrov <rafkadyrov@gmail.com>
2025-07-05 14:40:09 +0700

    updated scripts for splitting and augmenting data

--- utils/augment_train_split.py
+++ utils/augment_train_split.py
@@ -1,60 +1,102 @@
+"""This module contains a code for augmenting the training split of the dataset."""
+import argparse
+from pathlib import Path
+import shutil
+from typing import Optional
+
 import albumentations as A
 import cv2
+import yaml
 
 
-train_images_list_file_path = "../data/zebra_dataset/Train.txt"
-with open(train_images_list_file_path, "r") as train_filenames_file:
-    train_images_paths = train_filenames_file.read().splitlines()
+def main(dataset_path: Path, output_dataset_path: Optional[Path]):
+    if not dataset_path.exists():
+        raise FileNotFoundError(
+            f"The dataset path {dataset_path} does not exist. "
+            f"Please make sure you have the dataset in the correct location."
+        )
+    dataset_folder_to_use = Path(dataset_path)
+    if output_dataset_path is not None:
+        output_dataset_path = Path(output_dataset_path)
+        shutil.copytree(dataset_path, output_dataset_path)
+        loader = yaml.FullLoader
+        with open(dataset_path / "data.yaml") as dataset_description_file:
+            dataset_yaml = yaml.load(dataset_description_file, Loader=loader)
 
-train_labels = []
-for train_image_path in train_images_paths:
-    label_path = train_image_path.replace("images", "labels").replace(".jpg", ".txt")
-    train_labels.append(label_path)
+        output_yaml = {
+            "train": "images/Train",
+            "val": "images/Val",
+            "test": "images/Test",
+            "names": dataset_yaml["names"],
+            "path": dataset_yaml["path"].replace(dataset_path.name, output_dataset_path.name),
+        }
 
-transforms_list = [
-    A.AtLeastOneBBoxRandomCrop(width=256, height=256, p=1.0),
-    A.HorizontalFlip(p=0.5),
-    A.RandomBrightnessContrast(p=0.2),
-    A.Affine(scale=(0.8, 1.2),      # Zoom in/out by 80-120%
-            rotate=(-15, 15),      # Rotate by -15 to +15 degrees
-            # translate_percent=(0, 0.1), # Optional: translate by 0-10%
-            # shear=(-10, 10),          # Optional: shear by -10 to +10 degrees
-            p=0.7
-        ),
-    A.Perspective(p=0.5)
-]
-train_transform = A.Compose(transforms_list,
-                            bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
+        with open(output_dataset_path / "data.yaml", 'w') as file:
+            yaml.dump(output_yaml, file)
+        dataset_folder_to_use = Path(output_dataset_path)
 
-for image_path, label_path in zip(train_images_paths, train_labels):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    class_labels = []
-    bboxes = []
-    with open(label_path, "r") as label_file:
-        for line in label_file:
-            split_line = line.strip().split()
-            class_labels.append(int(line[0]))
-            bbox = [float(x) for x in split_line[1:]]
-            bboxes.append(bbox)
+    train_images_path = dataset_folder_to_use / "images/Train"
 
-    augmented = train_transform(image=image, bboxes=bboxes, class_labels=class_labels)
-    transformed_image = augmented['image']
-    transformed_bboxes = augmented['bboxes']
-    transformed_class_labels = augmented['class_labels']
+    transforms_list = [
+        A.AtLeastOneBBoxRandomCrop(width=256, height=256, p=1.0),
+        A.Rotate(p=0.5),
+        A.HorizontalFlip(p=0.5),
+        A.RandomBrightnessContrast(p=0.2),
+        A.Affine(scale=(0.8, 1.2), rotate=(-15, 15), p=0.7),
+        A.Perspective(p=0.5)
+    ]
+    train_transform = A.Compose(
+        transforms_list,
+        bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']),
+        seed=42
+    )
 
-    image_filename = image_path.split("/")[-1]
-    new_image_path = image_path.replace(image_filename, f"augmented_{image_filename}")
-    new_label_path = new_image_path.replace("images", "labels").replace(".jpg", ".txt")
+    for train_image_path in train_images_path.glob("*"):
+        image_path = str(train_image_path)
+        label_path = image_path.replace("images", "labels").replace(".jpg", ".txt")
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        class_labels = []
+        bboxes = []
+        with open(label_path, "r") as label_file:
+            for line in label_file:
+                split_line = line.strip().split()
+                class_labels.append(int(line[0]))
+                bbox = [float(x) for x in split_line[1:]]
+                bboxes.append(bbox)
+        for i in range(2):
+            augmented = train_transform(image=image, bboxes=bboxes, class_labels=class_labels)
+            transformed_image = augmented['image']
+            transformed_bboxes = augmented['bboxes']
+            transformed_class_labels = augmented['class_labels']
 
-    cv2.imwrite(new_image_path, cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR))
-    with open(new_label_path, "w") as new_label_file:
-        for label_num, bbox in enumerate(transformed_bboxes):
-            new_label_file.write(f"{transformed_class_labels[label_num]} {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}\n")
+            image_filename = image_path.split("/")[-1]
+            new_image_path = image_path.replace(image_filename, f"augmented_{i}_{image_filename}")
+            new_label_path = new_image_path.replace("images", "labels").replace(".jpg", ".txt")
 
-    with open(train_images_list_file_path, "a") as train_filenames_file:
-        train_filenames_file.write(new_image_path + "\n")
+            cv2.imwrite(new_image_path, cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR))
+            with open(new_label_path, "w") as new_label_file:
+                for label_num, bbox in enumerate(transformed_bboxes):
+                    new_label_file.write(
+                        f"{transformed_class_labels[label_num]}"
+                        f" {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}\n"
+                    )
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset_path",
+        type=Path,
+        default=Path("data/zebra_dataset"),
+        help="Path to the dataset folder. By default, it is set to data/zebra_dataset"
+    )
+    parser.add_argument(
+        "--output_dataset_path",
+        default=None,
+        help="Path to the output dataset folder. If not provided, augmented images will be added to"
+             " the original dataset."
+    )
 
-
+    args = parser.parse_args()
+    main(args.dataset_path, args.output_dataset_path)
--- utils/split_train_val_test.py
+++ utils/split_train_val_test.py
@@ -1,9 +1,10 @@
+"""This module contains a script for splitting exported dataset into train, val and test sets."""
+from argparse import ArgumentParser
 from pathlib import Path
 import shutil
 import sys
 from typing import List
 
-from sklearn.model_selection import train_test_split
 import yaml
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
@@ -19,83 +20,121 @@ def create_split(split_name: str,
                  output_labels_folder_path: Path,
                  output_images_folder_path: Path,
                  split_filenames_paths: List[str]):
+    """Creates a split of dataset by provided filenames.
+
+    Args:
+        split_name: The name of the split (e.g. "Train", "Val", "Test").
+        images_folder_path: A path to folder containing dataset images.
+        labels_folder_path: A path to folder containing dataset labels.
+        output_labels_folder_path: A path to folder for saving split labels.
+        output_images_folder_path: A path to folder for saving split images.
+        split_filenames_paths: A list of paths to images for related split.
+    """
     create_path_if_not_exists(output_labels_folder_path / split_name)
     create_path_if_not_exists(output_images_folder_path / split_name)
-    split_images_paths = []
     for split_image_filename in split_filenames_paths:
         image_filename = split_image_filename.split("/")[-1]
         image_file_path = output_images_folder_path / split_name / image_filename
         shutil.copyfile(src=images_folder_path / image_filename,
                         dst=image_file_path)
-        split_images_paths.append(image_file_path)
 
         label_path = split_image_filename.replace("images", "labels").replace(".jpg", ".txt")
         label_filename = label_path.split("/")[-1]
         label_file_path = output_labels_folder_path / split_name / label_filename
         shutil.copyfile(src=labels_folder_path / label_filename,
                         dst=label_file_path)
-    split_images_files_path = output_images_folder_path.parent / f"{split_name}.txt"
-    with open(split_images_files_path, "w") as split_images_list_file:
-        for image_path in split_images_paths:
-            split_images_list_file.write(str(image_path) + "\n")
 
 
-images_folder = ROOT_DIR / Path("data/frames")
-output_dataset_folder = ROOT_DIR / Path("data/zebra_dataset")
-labels_data_path = ROOT_DIR / Path("data/zebra_exported_dataset")
-labels_folder = labels_data_path / "labels/Train/frames"
-data_yaml_filename = labels_data_path / "data.yaml"
-train_filenames_filename = labels_data_path / "Train.txt"
+def main(path_to_exported_dataset: Path, path_to_frames: Path, path_to_output_dataset: Path):
+    images_folder = ROOT_DIR / path_to_frames
+    labels_data_path = ROOT_DIR / path_to_exported_dataset
+    output_dataset_folder = ROOT_DIR / path_to_output_dataset
+    labels_folder = labels_data_path / "labels/Train/frames"
+    data_yaml_filename = labels_data_path / "data.yaml"
+    train_filenames_filename = labels_data_path / "Train.txt"
 
-create_path_if_not_exists(output_dataset_folder)
+    create_path_if_not_exists(output_dataset_folder)
 
-with open(train_filenames_filename, "r") as train_filenames_file:
-    train_paths = train_filenames_file.read().splitlines()
+    with open(train_filenames_filename, "r") as train_filenames_file:
+        exported_files_paths = train_filenames_file.read().splitlines()
 
-train_val_paths = []
-test_filenames_paths = []
-for image_path in train_paths:
-    image_filename = image_path.split("/")[-1]
-    if image_filename.startswith("4_"):
-        test_filenames_paths.append(image_path)
-    elif not image_filename.startswith("4_1"):
-        train_val_paths.append(image_path)
+    train_filenames_paths = []
+    val_filenames_paths = []
+    test_filenames_paths = []
+    for image_path in exported_files_paths:
+        image_filename = image_path.split("/")[-1]
+        if image_filename.startswith("4_"):
+            test_filenames_paths.append(image_path)
+        elif image_filename.startswith("3_1_"):
+            val_filenames_paths.append(image_path)
+        else:
+            train_filenames_paths.append(image_path)
 
-train_filenames_paths, val_filenames_paths = train_test_split(train_val_paths,
-                                                              test_size=0.25,
-                                                              shuffle=True)
+    output_labels_path = output_dataset_folder / "labels"
+    output_images_path = output_dataset_folder / "images"
 
-output_labels_path = output_dataset_folder / "labels"
-output_images_path = output_dataset_folder / "images"
+    create_split("Train",
+                 images_folder,
+                 labels_folder,
+                 output_labels_path,
+                 output_images_path,
+                 train_filenames_paths)
 
-create_split("Train",
-             images_folder,
-             labels_folder,
-             output_labels_path,
-             output_images_path,
-             train_filenames_paths)
+    create_split("Val",
+                 images_folder,
+                 labels_folder,
+                 output_labels_path,
+                 output_images_path,
+                 val_filenames_paths)
 
-create_split("Val",
-             images_folder,
-             labels_folder,
-             output_labels_path,
-             output_images_path,
-             val_filenames_paths)
+    create_split("Test",
+                 images_folder,
+                 labels_folder,
+                 output_labels_path,
+                 output_images_path,
+                 test_filenames_paths)
 
-create_split("Test",
-             images_folder,
-             labels_folder,
-             output_labels_path,
-             output_images_path,
-             test_filenames_paths)
+    loader = yaml.FullLoader
+    with open(data_yaml_filename) as dataset_description_file:
+        dataset_yaml = yaml.load(dataset_description_file, Loader=loader)
 
-loader = yaml.FullLoader
-with open(data_yaml_filename) as dataset_description_file:
-    dataset_yaml = yaml.load(dataset_description_file, Loader=loader)
+    output_yaml = {
+        "train": "images/Train",
+        "val": "images/Val",
+        "test": "images/Test",
+        "names": dataset_yaml["names"],
+        "path": str(output_dataset_folder)
+    }
 
-output_yaml = {"train": "Train.txt", "val": "Val.txt", "test": "Test.txt"}
-output_yaml["names"] = dataset_yaml["names"]
-output_yaml["path"] = "."
+    with open(output_dataset_folder / "data.yaml", 'w') as file:
+        yaml.dump(output_yaml, file)
 
-with open(output_dataset_folder / "data.yaml", 'w') as file:
-    yaml.dump(output_yaml, file)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--path_to_exported_dataset",
+        type=Path,
+        default=Path("data/zebra_exported_dataset"),
+        required=False,
+        help="Path to labelled dataset exported from CVAT in YOLO format. By default, the "
+             "path is set to data/zebra_exported_dataset"
+    )
+    parser.add_argument(
+        "--path_to_frames",
+        type=Path,
+        default=Path("data/frames"),
+        required=False,
+        help="Path to folder containing frames from video. By default, "
+             "the path is set to data/frames"
+    )
+    parser.add_argument(
+        "--path_to_output_dataset",
+        type=Path,
+        default=Path("data/zebra_dataset"),
+        required=False,
+        help="Path to folder for saving split data as a new dataset. By default, "
+             "the path is set to data/zebra_dataset"
+    )
+    args = parser.parse_args()
+    main(args.path_to_exported_dataset, args.path_to_frames, args.path_to_output_dataset)
